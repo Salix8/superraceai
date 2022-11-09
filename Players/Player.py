3c4291e55a2de29b994e3a2f686665be9bbe4e93
SaulPachecoTrilles <al427443@uji.es>
2022-11-09 11:48:45 +0100

    first commit

--- Players/Player.py
+++ Players/Player.py
@@ -1,3 +1,6 @@
 class Player:
     def __init__(self):
         print("Player is inicializate")
+
+    def run(self, player):
+        print("Game is running")
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Game.Game import Game
 from Players.Player import Player
 
 if __name__ == '__main__':
-     game = Game()
-     player = Player()
+    game = Game()
+    player = Player()
 
     game.run(player)
